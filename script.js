3414698120755a8d9f55ad494aebd113c12b5892
Paige Thai <paigegiathai@gmail.com>
2023-02-20 17:23:33 -0500

    added JS functionality for hamburger menu to drop down nav bar

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("hello world");
+
+// PSUDEO CODE
+    // 1) User clicks on hamburger icon to open drop-down navigation menu 
+    // 2) Event listener listens for user's click
+    
+
+const hamburgerButton = document.querySelector(".hamburger");
+// console.log(hamburgerButton);
+
+const hamburgerMenu = document.querySelector(".ham-links");
+// console.log(hamburgerMenu);
+
+hamburgerButton.addEventListener("click", function() {
+    if (hamburgerMenu.style.display === "block") {
+        hamburgerMenu.style.display = "none";
+    } else {
+        hamburgerMenu.style.display = "block";
+    }
+});
+
+// Reveresed engineered this to work! Thank you to W3 Schools for the tutorial! 
+// https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
